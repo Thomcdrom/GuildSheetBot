bc94da41cc691dd69fa8e0190c88d636131605de
thomcdrom <thomcdrom@gmail.com>
2018-08-01 18:06:00 +0200

    Added .env support
    Added .env support for the discord bot key

--- Source/App.ts
+++ Source/App.ts
@@ -1,7 +1,13 @@
 import {Startup} from "./Startup" ;
+import * as env from "dotenv";
+
 class Bot {
-    public static start(botkey :string = null, sheetkey :string = null ) {
+    constructor() {
+        env.config();
+    }
+
+    public static start(botkey :string = process.env.DISCORD_TOKEN, sheetkey :string = null ) {
        const bot = new Startup(botkey, sheetkey);          
     }
 }
-Bot.start('NDIwMjczNjUxMDkxMjQzMDEz.DkJX7A.PqvK60uMwfU_lvj-ed3JYmHj7f');
+Bot.start();
