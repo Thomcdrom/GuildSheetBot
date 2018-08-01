08e97263d758529f3475c3952106a2b10496a8e9
thomcdrom <thomcdrom@gmail.com>
2018-08-01 17:12:23 +0200

    added error handeling
    Added error handeling for a bad login with discord.js API

--- Source/App.ts
+++ Source/App.ts
@@ -1,13 +1,7 @@
 import {Startup} from "./Startup" ;
 class Bot {
     public static start(botkey :string = null, sheetkey :string = null ) {
-        try {
-            const bot = new Startup(botkey, sheetkey);          
-        }
-        catch (error) {
-            console.log(`something shit the bed: ${error}`)
-        }
+       const bot = new Startup(botkey, sheetkey);          
     }
 }
-
-const status = Bot.start('NDIwMjczNjUxMDkxMjQzMDEz.DkJX7A.PqvK60uMwfU_lvj-ed3JYmHj7f8');
+Bot.start('NDIwMjczNjUxMDkxMjQzMDEz.DkJX7A.PqvK60uMwfU_lvj-ed3JYmHj7f');
--- Source/Startup.ts
+++ Source/Startup.ts
@@ -4,15 +4,14 @@ export class Startup {
       client: discord.Client
 
     constructor(botkey :string, sheetkey :string) {
-        try{
         this.client = new discord.Client();
-        this.client.login(botkey);
+        this.client.login(botkey).catch(() => {console.error('Your discord bot key isn\'t valid')});
+        
+        this.read();
+    }  
+    private read() {
         this.client.on('ready', () => {
             console.log(`Logged in as ${this.client.user.tag}!`);
         });
-        }
-        catch {
-            console.error('You provided a wrong bot token or and bad SheetsAPI token ');
-        }
-    }  
+    }
 }
