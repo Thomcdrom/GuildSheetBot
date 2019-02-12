fd504cfdfbb3d8cec1ee3564d057c2e92d3f711c
Thom van Heeswijk <thom@icreate.nl>
2019-02-12 15:13:29 +0100

    read sheet now works
    decided to used callbacks due to simplicity, will need to make this async later in the process when it starts getting slow

--- Source/Gdocs/Autenticate.ts
+++ Source/Gdocs/Autenticate.ts
@@ -1,6 +1,7 @@
 import { readFile, writeFile } from 'fs';
-import {google} from 'googleapis';
+import {google, speech_v1p1beta1} from 'googleapis';
 import * as readline from 'readline';
+import { OAuth2Client } from 'googleapis-common';
 
 let SCOPES = ['https://www.googleapis.com/auth/spreadsheets']; //you can add more scopes according to your permission need. But in case you chang the scope, make sure you deleted the ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json file
 const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/'; //the directory where we're going to save the token
@@ -8,37 +9,38 @@ const TOKEN_PATH = TOKEN_DIR + 'token_AutomationBot.json'; //the file which will
 
 export abstract class GdocAutenticate {
 
-   protected sheetId :string;
+   private sheetId :string;
    protected authenthication :any;
     
     constructor() {
         this.sheetId = process.env.SHEET_ID;
     }
 
-    protected authenticate(){
-     readFile('credentials.json', async (err, content :Buffer) => {
+protected authenticate(){
+     readFile('credentials.json', (err, content :Buffer) => {
         if (err) return console.log('Error loading client secret file:', err);
         // Authorize a client with credentials, then call the Google Sheets API.
-        this.authenthication = this.authorize(JSON.parse(content.toString('utf8')));
-        this.run();
+        this.authorize(JSON.parse(content.toString('utf8')), this.run);
       });
       
   }
 
- private authorize(credentials) {
+ private authorize(credentials, callback) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
      readFile(TOKEN_PATH, (err, token :any) => {
-        if (err) return this.getNewToken(oAuth2Client);
+        if (err) return this.getNewToken(oAuth2Client, callback);
         oAuth2Client.setCredentials(JSON.parse(token.toString('utf8')));
+
+        callback(oAuth2Client, this.sheetId);
     });
 
   }
 
-  private getNewToken(oAuth2Client) {
+  private getNewToken(oAuth2Client, callback) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -58,11 +60,12 @@ export abstract class GdocAutenticate {
             if (err) console.error(err);
             console.log('Token stored to', TOKEN_PATH);
           });
+          callback(oAuth2Client, this.sheetId);
         });
       });
   }
 
-   run() {
+   run(auth: OAuth2Client, id :string ) {
     console.log('I SHOULD BE OVERWRITTEN!');
   };
 }
--- Source/Gdocs/Signup.ts
+++ Source/Gdocs/Signup.ts
@@ -1,37 +1,36 @@
 import { GdocAutenticate } from './Autenticate';
 import { google } from 'googleapis';
 import { User } from 'discord.js';
+import { OAuth2Client } from 'googleapis-common';
 
 export class Signup extends GdocAutenticate {
 
     user :User;
     value :string;
 
-    constructor(user :User ,value :string) {
+    constructor(user :any ,value :string) {
         super();
         this.user = user;
         this.value = value;
 
+        this.run = this.run.bind(this);
+
         this.authenticate();
     }
 
-    run() {
-        const auth = this.authenthication;
-
-        console.log(auth);
-
+    run(auth: OAuth2Client, id :string) {
         const sheets = google.sheets({version: 'v4', auth });
+
         sheets.spreadsheets.values.get({
-            spreadsheetId: this.sheetId,
-            range: "rooster!B1:B",
+            spreadsheetId: id,
+            range: "rooster!B1:C72",
         }, (err, res) => {
             if (err) return console.log('The API returned an error: ' + err);
             const rows = res.data.values;
             if (rows.length) {
-            console.log('Name, Major:');
             // Print columns A and E, which correspond to indices 0 and 4.
             rows.map((row) => {
-                console.log(`${row[0]}, ${row[4]}`);
+                this.user.lastMessage.channel.sendMessage(`${row[0]}, ${row[1]}`);
             });
             } else {
             console.log('No data found.');
